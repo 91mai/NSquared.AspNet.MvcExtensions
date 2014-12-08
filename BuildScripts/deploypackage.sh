7b3fd0b0d01bc5af5efc0795fc87adc149f4f190
Kirk Chen <kirkchen@nine-yi.com>
2014-12-08 13:01:11 +0800

    Modify nuspec path

--- BuildScripts/deploypackage.sh
+++ BuildScripts/deploypackage.sh
@@ -4,6 +4,6 @@ VersionNumber="0.0.${TRAVIS_BUILD_NUMBER}-alpha"
 echo "VersionNumber: $VersionNumber"
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then         
-     mono --runtime=v4.0.30319 .nuget/NuGet.exe Pack NugetSpecs/NSquared.MvcExtensions.nuspec -NonInteractive -Version ${VersionNumber}
+     mono --runtime=v4.0.30319 .nuget/NuGet.exe Pack NSquared.MvcExtensions.nuspec -NonInteractive -Version ${VersionNumber} 
      mono --runtime=v4.0.30319 .nuget/NuGet.exe Push NSquared.MvcExtensions.${VersionNumber}.nupkg $NUGET_APIKEY -NonInteractive     
 fi
