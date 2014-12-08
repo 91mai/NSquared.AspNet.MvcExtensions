5a0ba54a7ec74f9f892864e2049e0fe8f5cb8ce3
Kirk Chen <kirkchen@nine-yi.com>
2014-12-08 11:09:36 +0800

    Modify nuget install output folder

--- BuildScripts/unittest.sh
+++ BuildScripts/unittest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mono --runtime=v4.0.30319 .nuget/NuGet.exe install .nuget/packages.config
+mono --runtime=v4.0.30319 .nuget/NuGet.exe install .nuget/packages.config -o packages
 mono --runtime=v4.0.30319 packages/SpecRun.Runner.1.2.0/tools/SpecRun.exe run NSquared.MvcExtensions.Test.dll /baseFolder:MvcExtensions.Test/bin/Debug /toolIntegration:vs2010 /reportFile:.\TestReport.html
