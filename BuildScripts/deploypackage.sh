028eb8b41c8dbc35fa897d7ddb2eb6a43e9cb72e
Kirk Chen <kirkchen@nine-yi.com>
2014-12-08 13:54:16 +0800

    Fix deploy script

--- BuildScripts/deploypackage.sh
+++ BuildScripts/deploypackage.sh
@@ -4,6 +4,6 @@ VersionNumber="0.0.${TRAVIS_BUILD_NUMBER}-alpha"
 echo "VersionNumber: $VersionNumber"
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then         
-     mono --runtime=v4.0.30319 .nuget/NuGet.exe Pack NSquared.MvcExtensions.nuspec -NonInteractive -Version ${VersionNumber} -o .\NugetSpecs
-     mono --runtime=v4.0.30319 .nuget/NuGet.exe Push .\NugetSpecs\NSquared.MvcExtensions.${VersionNumber}.nupkg $NUGET_APIKEY -NonInteractive     
+     mono --runtime=v4.0.30319 .nuget/NuGet.exe Pack NSquared.MvcExtensions.nuspec -NonInteractive -Version ${VersionNumber} 
+     mono --runtime=v4.0.30319 .nuget/NuGet.exe Push NSquared.MvcExtensions.${VersionNumber}.nupkg $NUGET_APIKEY -NonInteractive     
 fi
